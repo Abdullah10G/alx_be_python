f17b7deed2d5aeaf1152bfe7495b67eff1c17bb0
Abdullah10G <135840129+Abdullah10G@users.noreply.github.com>
2024-07-29 17:32:52 +0300

    Delete python_introduction/future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++
@@ -1,3 +0,0 @@
-v =int(input("How old are you :"))      # assuming we are in 2023
-age = v + 23                          # we want to calculate the age in 2050
-print("In 2050, you will be ",age,"years old")
